b68a1d60f2ef4a62b0eaaabf2c6211f4d52103b6
leandropinheiroo <leandropinheiro98@gmail.com>
2022-12-29 16:57:55 -0300

    Feature | Project architecture

---
+++ GeekShopping/GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Model;
+using GeekShopping.ProductAPI.Repository;
+using GeekShopping.ProductAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductService _productService;
+
+        public ProductController(ProductService productService)
+        {
+            _productService = productService;  
+        }
+
+        [HttpGet]
+        public ActionResult<List<Product>> FindAll()
+        {
+            return Ok(_productService.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ProductVO> FindById(long id)
+        {
+            return Ok(_productService.FindById(id));
+        }
+    }
+}
---
+++ GeekShopping/GeekShopping.ProductAPI/Startup/DependencyInjecttionSetup.cs
@@ -0,0 +1,38 @@
+﻿using GeekShopping.ProductAPI.Config;
+using GeekShopping.ProductAPI.Repository;
+using GeekShopping.ProductAPI.Services;
+
+namespace GeekShopping.ProductAPI.Startup
+{
+    public static class DependencyInjecttionSetup
+    {
+        public static IServiceCollection Register(this IServiceCollection services)
+        {
+            services.AddSingleton(MappingConfig.RegisterMaps().CreateMapper());
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddEndpointsApiExplorer();
+
+            services.AddControllers();
+            services.AddSwaggerGen(swagger =>
+            {
+                swagger.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Geek Shopping", Version = "v1" });
+            });
+            //Register all Repositories Injection
+            services.RegisterRepository();
+            //Register all Services Injection
+            services.RegisterServices();
+            return services;
+        }
+
+        public static void RegisterRepository(this IServiceCollection services)
+        {
+            services.AddScoped<ProductRepository>();
+        }
+
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<ProductService>();
+        }
+    }
+}
